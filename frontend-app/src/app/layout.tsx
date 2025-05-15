9930c69307b33a19f006570672b1bc6b5519bfe8
Ajin <ajinkp9745@gmail.com>
2025-05-15 16:17:04 +0530

    Add PWA support with manifest and service worker files

--- frontend-app/src/app/layout.tsx
+++ frontend-app/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/frontend-app/public/gaming.png" />
+        <link rel="apple-touch-icon" href="/gaming.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
